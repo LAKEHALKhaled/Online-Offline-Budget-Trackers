cd3aa700540b35e03ff6ca725b9479ee01f5aa5b
KhaledLakehal <khaled.lakehal93@gmail.com>
2021-08-20 19:07:36 -0400

    added db.js and update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const logger = require("morgan");
+// const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
+// const compression = require("compression");
 
 const PORT = 3001;
 
 const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
-app.use(compression());
+// app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
